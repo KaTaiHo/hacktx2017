5c635c830ce763c2f8b8abde67f4b1c8d67c52aa
David Bush <David.C.Bush@outlook.com>
2017-10-28 14:47:46 -0500

    Added base activity disaster selection action and view

---
+++ app/src/main/java/com/example/kataiho/hacktx2017/DisasterSelection.java
@@ -0,0 +1,13 @@
+package com.example.kataiho.hacktx2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisasterSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disaster_selection);
+    }
+}
